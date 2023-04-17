f170b288e056c59f0fac40c91745a99ea1f9db4b
Lucy Zighe <lucy.zighe@student.moringaschool.com>
2023-04-17 16:23:17 +0300

    rename index.html

---
+++ index.js/akan.js
@@ -0,0 +1,11 @@
+//Grabbing single DOM elements
+//let form=document.queryselector
+console.log(Form Data)
+const male names = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+document.getElementById("list").innerHTML = male names ;
+const Names= ["Kwasi", "Kwadwo", "Kwabena", "Kwaku","Yaw","Kofi","Kwame"];
+console.log(male names length
+//document.getElementById("list").innerHTML = female names ;
+const Names =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+
+const d = new Date(year,month,day);
